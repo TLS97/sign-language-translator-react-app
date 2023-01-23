e08a5c71c396bd4bf2710a778a93104950fb4077
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-01-23 12:04:03 +0100

    chore: setup React Router with routes for the login, profile and translation pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./views/Login";
+import Profile from "./views/Profile";
+import Translations from "./views/Translations";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/translations" element={<Translations />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
