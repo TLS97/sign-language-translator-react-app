4d3c7b5c3d9a1afce3473cbf0c5c9ceb5e1ce288
Ha <Hahoang@live.no>
2023-01-24 18:40:38 +0100

    feat: Add login form with Context Api

---
+++ src/utils/storage.js
@@ -0,0 +1,27 @@
+const validateKey = key => {
+    if(!key || typeof key !== 'string'){
+        throw new Error('Invalid storage key provided')
+    }
+}
+
+export const storageSave = (key, value) => {
+   validateKey(key)
+    if(!value){
+        throw new Error('storageSave: No value provided for ' + key)
+    }
+    sessionStorage.setItem(key, JSON.stringify(value))
+}
+
+export const storageRead = key => {
+   validateKey(key)
+    const data = sessionStorage.getItem(key)
+    if (data) {
+        return JSON.parse(data)
+    }
+    return null
+}
+
+export const storageDelete = key => {
+    validateKey(key)
+    sessionStorage.removeItem(key)
+}
