4e9b41ead80d8cdccea34b1db7a7766364aeb23a
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-01-27 10:11:23 +0100

    chore: removing redux setup since we're not using it

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./state/store";
-import { Provider } from "react-redux";
 import AppContext from "./context/AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppContext>
-        <App />
-      </AppContext>
-    </Provider>
+    <AppContext>
+      <App />
+    </AppContext>
   </React.StrictMode>
 );
 
